8fd8771df14d937a47f5e1c793d63c2b0b3b6bb0
acelove <chenq47@mail2.sysu.edu.cn>
2017-11-10 16:32:45 +0800

    Update feats.py

--- code/feats.py
+++ code/feats.py
@@ -49,9 +49,8 @@ def deal_Age(train, test):
 def deal_SibSp_and_Parch(train,test):
 
     for data in [train,test]:
-        data['Fam_Size'] = np.where((data['SibSp']+data['Parch']) == 0 , 'Solo',
-                            np.where((data['SibSp']+data['Parch']) <= 3,'Nuclear',
-                            'Big'))
+        data['Fam_Size'] = data['SibSp'] + data['Parch']
+        data['Fam_Size'] = data['Fam_Size'].apply(lambda x: 'Solo' if x==0 else ('Normal' if x<=3 else 'Big'))
 
     dummies_Fam_Size_train, dummies_Fam_Size_test = get_dummies(train['Fam_Size'],test['Fam_Size'],prefix='Fam_Size')
     train = pd.concat([train, dummies_Fam_Size_train],axis=1)
@@ -65,9 +64,7 @@ def deal_Ticket(train,test):
 
     for data in [train,test]:
         data['Ticket_Lett'] = data['Ticket'].apply(lambda x: str(str(x)[0]))
-        data['Ticket_Lett'] = np.where((data['Ticket_Lett']).isin(['1', '2', '3', 'S', 'P', 'C', 'A']), data['Ticket_Lett'],
-                                    np.where((data['Ticket_Lett']).isin(['W', '4', '7', '6', 'L', '5', '8']),
-                                            'Low_ticket', 'Other_ticket'))
+        data['Ticket_Lett'] = data['Ticket_Lett'].apply(lambda x : x if x in ['1', '2', '3', 'S', 'P', 'C', 'A'] else ('Low_ticket' if x in ['W', '4', '7', '6', 'L', '5', '8'] else 'Other_ticket'))
         data['Ticket_Len'] = data['Ticket'].apply(lambda x: len(x))
 
     dummies_Ticket_Lett_train, dummies_Ticket_Lett_test = get_dummies(train['Ticket_Lett'],test['Ticket_Lett'],prefix='Ticket_Lett')
